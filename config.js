23e9b56c0c55d8d97683df4b94dc62a16965fe08
lisalisa122051-droid <lisalisa122051@gmail.com>
2025-12-25 21:09:29 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,29 @@
+global.owner = ['628xxxxxxx'] // Ganti dengan nomor Anda
+global.botName = 'Bot Name'
+global.ownername = 'Owner Name'
+global.prefa = ['!', '/', '.', '#']
+global.sessionName = 'session'
+global.packname = 'Sticker Pack'
+global.author = '© Bot Name'
+global.mess = {
+    wait: 'Tunggu sebentar...',
+    success: 'Berhasil!',
+    admin: 'Fitur khusus admin grup!',
+    botAdmin: 'Bot harus menjadi admin!',
+    owner: 'Fitur khusus owner!',
+    group: 'Fitur hanya untuk grup!',
+    private: 'Fitur hanya untuk chat private!',
+    bot: 'Fitur khusus pengguna bot!',
+    error: 'Terjadi kesalahan!'
+}
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+
+global.multiplier = 69
+
+global.mongoDB = '' // Jika ingin menggunakan MongoDB
+
+module.exports = { owner }
